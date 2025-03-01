e83fdbaa3ac2957fcf96d5d79e558e470ceba22f
ayyooba <ayyooba@fortude.co>
2025-03-02 00:53:40 +0530

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { PaymentModule } from './payment/payment.module';
-import { PaymentMethodType } from './type';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({}),
-    PaymentModule.register(process.env.PAYMENT_METHOD as PaymentMethodType)
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
